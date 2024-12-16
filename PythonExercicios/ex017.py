9125b235704e6e762769493ffeb909be466e7d68
Amanda Amorim ʚĭɞ <amandavsadev@gmail.com>
2024-12-15 21:16:25 -0300

    Add files via upload

---
+++ PythonExercicios/ex017.py
@@ -0,0 +1,8 @@
+import math
+
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+
+h = math.hypot(co,ca)
+
+print('A hipotenusa vai medir {:.2f}'.format(h))
