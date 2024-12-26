c3f365214bdf765de29919816a3da9f665f5e81c
Amanda Amorim ʚĭɞ <amandavsadev@gmail.com>
2024-12-26 15:26:16 -0300

    Delete ex013.py

--- ex013.py
+++
@@ -1,4 +0,0 @@
-salario = float(input('Qual é o salário do funcionário? R$'))
-aumento = (15/100) * salario
-
-print('Um funcionário que ganhava R${}, com 15% de aumento, passa a receber R${:.2f}.'.format(salario, salario+aumento))
