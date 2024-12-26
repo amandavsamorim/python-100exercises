a51bc4eeb2104edf6d0be0bf9ccadb5746994903
Amanda Amorim ʚĭɞ <amandavsadev@gmail.com>
2024-12-26 15:30:46 -0300

    Add files via upload

---
+++ exercises/ex005.py
@@ -0,0 +1,5 @@
+#Antecessor e sucessor
+
+n1 = int(input('Digite um número: '))
+
+print('Antecessor é {} e sucessor é {}'.format(n1 - 1, n1 + 1))
---
+++ exercises/ex008.py
@@ -0,0 +1,11 @@
+# Conversão centímetros e milímetros
+
+d = int(input('Uma distância em metros: '))
+print('A medida de {:.1f}m corresponde a'.format(d))
+
+print('{}km'.format(d / 1000))
+print('{}hm'.format(d / 100))
+print('{}dam'.format(d / 10))
+print('{}dm'.format(d * 10))
+print('{}cm'.format(d * 100))
+print('{}mm'.format(d * 1000))
