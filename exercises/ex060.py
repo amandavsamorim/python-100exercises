9b46a7354440f351f9e34b8827704317c402a7ed
Amanda Amorim ʚĭɞ <amandavsadev@gmail.com>
2025-01-29 22:23:40 -0300

    Add files via upload

---
+++ exercises/ex060.py
@@ -0,0 +1,15 @@
+from time import sleep
+n = int(input('Digite um número para calcular seu fatorial: '))
+c = n
+f = 1
+
+print('Calculando...')
+sleep(1)
+
+print('{}! = '.format(n), end='')
+while c > 0:
+   print('{}'.format(c), end='')
+   print(' x ' if c > 1 else ' = ', end='')
+   f *= c
+   c -= 1
+print('{}'.format(f))
